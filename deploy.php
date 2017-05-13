9380f3a686b5daa5d7049fc1f9a6c0cad7ad4e5a
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2017-05-13 10:44:01 -0300

    Ajuste da estrutura

--- deploy.php
+++ deploy.php
@@ -28,7 +28,7 @@ set('rollbar', [
 
 // Slack
 set('slack', [
-    'token' => 'xoxp-176584622293-177310698935-183962952999-bceb2c4991a023bcb1d780c4ed40c8e1',
+    'token' => 'xoxp-176584622293-177310698935-182577379713-8a2736d6a2e132f81b2fa05ed9f88e17',
     'team' => 'confphprs',
     'app' => 'app name',
 ]);
