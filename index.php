4c25bfd398ad8bd134de65d21616054a3ec3504b
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2017-05-13 09:45:42 -0300

    Ajuste da estrutura

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ use Php\Phprs\Calculadora\Soma;
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Cálculadora</title>
+    <title>Calculadora</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
